3e6e299db91363dc45797f5b4e3cdceeeeefd389
igorbag <igorbag@hotmail.com>
2019-11-17 15:38:58 -0200

    Adicionando rota de costumers

---
+++ routes/costumer.route.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const HttpStatusCodes = require('http-status-codes');
+const Costumer = require('../models/costumer.model');
+const route = express.Router();
+
+route.get('/costumers/:id', async (req, res, next) => {
+    try {
+      const costumer = await Costumer.getById(req.params.id);
+      if (costumer) {
+        res.send(costumer);
+      } else {
+        res.sendStatus(HttpStatusCodes.NOT_FOUND);
+      }
+    } catch (error) {
+      res.sendStatus(HttpStatusCodes.INTERNAL_SERVER_ERROR);
+    }
+  });
+
+
+  oute.get('/costumers', async (req, res, next) => {
+    try {
+      const costumer = await Costumer.getAll();
+      res.send(costumer);
+    } catch (error) {
+      res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).send(error);
+    }
+  });
+  
+  route.post('/costumers', async (req, res, next) => {
+    try {
+      await Costumer.create(req.body);
+      const responseBody = { message: 'Registro inserido com sucesso!' };
+      res.status(HttpStatusCodes.CREATED).send(responseBody);
+    } catch (error) {
+      res.status(HttpStatusCodes.BAD_REQUEST).send(error);
+    }
+  });
+  
+  route.put('/costumers/:id', async (req, res, next) => {
+    try {
+      req.body._id = req.params.id;
+      await Costumer.update(req.body);
+      const responseBody = { message: 'Registro atualizado com sucesso!' };
+      res.status(HttpStatusCodes.OK).send(responseBody);
+    } catch (error) {
+      res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).send(error);
+    }
+  });
+  
+  route.delete('/costumers/:id', async (req, res, next) => {
+    try {
+      await Costumer.delete(req.params.id);
+      const responseBody = { message: 'Registro removido com sucesso!' };
+      res.status(HttpStatusCodes.OK).send(responseBody);
+    } catch (error) {
+      res.status(HttpStatusCodes.INTERNAL_SERVER_ERROR).send(error);
+    }
+  });
+  
+  route.post('/costumers/:id/action', async (req, res, next) => {
+    try {
+      const deviceId = req.params.id;
+      const device = await User.getDeviceOfUser(req.user._id, deviceId);
+  
+      if (!device) {
+        res.sendStatus(HttpStatusCodes.NOT_FOUND);
+      } else {
+        mqtt.publish(device.deviceid, req.body.value);
+        res.status(HttpStatusCodes.OK).send();
+      }
+  
+    } catch (error) {
+      res.status(HttpStatusCodes.BAD_REQUEST).send(error);
+    }
+  });
+  
+  module.exports = route;
