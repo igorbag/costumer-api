a298048c8e56339520a8ea8bde16f51f9b12e76e
igorbag <igorbag@hotmail.com>
2019-11-17 15:21:37 -0200

    Adicionando model and schema of costumers / social network

---
+++ models/costumer.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const costumerSchema = require('../schemas/costumer.schema');
+const CostumerModel = mongoose.model('Costumer', costumerSchema, 'costumers');
+
+class Costumer {
+  static create(costumer) {
+    return CostumerModel.create(costumer);
+  }
+
+  static getById(_id) {
+    return CostumerModel.findOne({ _id }).lean();
+  }
+
+  static update(costumer) {
+    return CostumerModel.update({ _id: costumer._id }, costumer);
+  }
+
+  static delete(_id) {
+    return CostumerModel.deleteOne({ _id });
+  }
+
+  static getAll() {
+    return CostumerModel.find().lean();
+  }
+}
--- schemas/social.schema.js
+++ schemas/social.schema.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
-  name: { type: String, required: true, minlength: 5, maxlength: 50 },
+  name: { type: String, required: true, minlength: 5, maxlength: 50 , unique:true},
 });
 
 module.exports = schema;
